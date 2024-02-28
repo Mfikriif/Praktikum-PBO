b3816f88990e4ecf13c859ec04aff8799f74d9b3
Muhammad Fikri Firdaus <126538861+Mfikriif@users.noreply.github.com>
2024-02-28 10:08:15 +0700

    Add files via upload

---
+++ Praktikum 2/src/MGaris.java
@@ -0,0 +1,16 @@
+/**
+ * File     	 : Titik.java		28/02/24
+ * Nama penulis  : Muhammad Fikri Firdaus
+ * NIM           : 24060122140115
+ * Deskripsi     : Kelas yang berisi body dari MTitik.java
+ *
+ */
+public class MGaris {
+    public static void main(String[] args) {
+        Titik t1 =  new Titik(3,5);
+        Titik t2 = new Titik(7,8);
+        Garis garis1 = new Garis(t1,t2);
+
+
+    }
+}
