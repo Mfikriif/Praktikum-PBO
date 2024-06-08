1b8174c1efdfe210a9e5713f4600cb4034a80fae
Muhammad Fikri Firdaus <126538861+Mfikriif@users.noreply.github.com>
2024-06-08 19:18:05 +0700

    Add files via upload

---
+++ Praktikum 10/Proyek3/test/proyek3/proyek3NGTest.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/EmptyTestNGTest.java to edit this template
+ */
+package proyek3;
+
+import static org.testng.Assert.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class proyek3NGTest {
+    
+    public proyek3NGTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @BeforeMethod
+    public void setUpMethod() throws Exception {
+    }
+
+    @AfterMethod
+    public void tearDownMethod() throws Exception {
+    }
+
+    /**
+     * Test of main method, of class proyek3.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        proyek3.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of $$anonymousClasses method, of class proyek3.
+     */
+    @Test
+    public void test$$anonymousClasses() {
+        System.out.println("$$anonymousClasses");
+        proyek3 instance = new proyek3();
+        instance.$$anonymousClasses();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
